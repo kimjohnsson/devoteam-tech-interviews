453a4be0c7b93647728ad44f3b4cf8e78a822453
Kim Johnsson <kim.johnsson@telia.com>
2025-02-05 10:39:11 +0100

    fix: incorrect Question type key

--- web-quiz/vue-ts/src/types/game.ts
+++ web-quiz/vue-ts/src/types/game.ts
@@ -2,7 +2,7 @@ export type Question = {
   category: string;
   correct_answer: string;
   difficulty: string;
-  incorrect_answer: string[];
+  incorrect_answers: string[];
   question: string;
   type: string;
 };
