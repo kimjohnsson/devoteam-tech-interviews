a21431122cfd30dffade0266ba342966cb3f97f1
Kim Johnsson <kim.johnsson@telia.com>
2025-02-05 10:39:12 +0100

    tech: added react router

--- web-quiz/react-ts/src/App.tsx
+++ web-quiz/react-ts/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <>
-    <h1>React Web Quiz</h1>
+      <main>
+        <h1>React Web Quiz</h1>
+
+        <p>
+          10 questions about video games! Multiple choice one is right. Answer every question within
+          15 seconds. 2 life lines will be provided one 50/50 and one +10 s. Each lifeline can only
+          be used once during a game."
+        </p>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
